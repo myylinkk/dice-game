fb2109384dacc600e992c262f14f83af65bd5f44
Linh Tong <81243424+myylinkk@users.noreply.github.com>
2021-09-22 23:41:59 +0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,13 @@
+var randomNum1 = Math.floor(Math.random()* 6) + 1;
+document.querySelector(".img1").setAttribute("src", "images/dice" + randomNum1 + ".png");
+
+var randomNum2 = Math.floor(Math.random()* 6) + 1;
+document.querySelector(".img2").setAttribute("src", "images/dice" + randomNum2 + ".png");
+
+if (randomNum1 > randomNum2) {
+  document.querySelector("h1").innerHTML = "🎉Player1 wins!"
+} else if (randomNum1 < randomNum2) {
+  document.querySelector("h1").innerHTML = "Player2 wins🎉!"
+} else {
+  document.querySelector("h1").innerHTML = "Its a Draw!!"
+}
